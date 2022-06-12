046a8cccc6c74f31914a4991be96e89b855f7467
Zdragun <zdragunnazar@gmail.com>
2022-06-12 09:13:32 +0300

    complited UseRef

--- hookswork/src/components/UseRefHook.js
+++ hookswork/src/components/UseRefHook.js
@@ -1,26 +1,34 @@
-import React,{useState,useRef} from 'react'
+import React, { useState, useRef } from 'react'
 
 export const UseRefHook = () => {
-/*     const [numbers,setNumbers] = useState([1])] */
-    const [state,setState] = useState([])
-    const inpRef = useRef(null);
-    
-    const onClick = () =>
-    {
-       console.log(inpRef.current.value);
-       setState(inpRef.current.value)
-    }
-   
+  /*     const [numbers,setNumbers] = useState([1])] */
+  const [state, setState] = useState([])
+  const inpRef = useRef(null);
+
+  const onClick = () => {
+    console.log(inpRef.current.value);
+    setState([...state, inpRef.current.value])
+  }
+
+
   return (
-    
+
     <div>
-     <input  ref={inpRef} type="text" placeholder='Type Text' />  
-    
-    <button onClick={onClick} >add Number</button>
-    <ul >
-    <li>{state}</li>
-    </ul>
-   
-        </div>
+      <input
+        ref={inpRef}
+        type="text"
+        placeholder='Type Text'
+      />
+
+      <button onClick={onClick}>add Number</button>
+
+      <div>
+        <ul>{state.map((elem)=>(
+          <li key={elem}>{elem}</li>
+        ))}</ul>
+      </div>
+
+    </div>
+
   )
 }
